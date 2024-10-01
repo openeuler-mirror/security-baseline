64d58e5881a50599fc7a3f82181d183528cd70ea
wuksh <wuksh@chinatelecom.cn>
2024-10-01 18:14:10 +0800

    add some basefunction and shell calling modules.

---
+++ security-baseline/do_shell.py
@@ -0,0 +1,13 @@
+#提供python执行shell脚本的功能,并输出
+import subprocess
+import os
+
+#input: run_shell(command，True/False) #单条shell指令，是否显示输出结果
+def run_shell(do,show_state=True):
+    result=os.popen(do)
+    output=[]
+    for l in result.readlines():
+        output.append(l.split('\n')[0])
+        if show_state:
+            print(output[-1])
+    return output
