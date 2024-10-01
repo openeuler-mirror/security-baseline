656671bb1cf80b427a9d192114801bc04207f5ad
wuksh <wuksh@chinatelecom.cn>
2024-10-01 18:28:59 +0800

    Add reinforcement items to format display using prettytable.

--- security-baseline/base_function.py
+++ security-baseline/base_function.py
@@ -35,7 +35,7 @@ class BaseFix():
 
     def check(self):
         pass
-    
+
 
 def split_file_by_line(file,sym,condi,condi_order,out_order): #(`awk -F ":" '( $2 == "" ) { print $1 }' /etc/shadow`)
     #按行检索文档，并以sym作为分割，返回第order个分割结果等于condi的所有结果
@@ -72,7 +72,7 @@ def cp_file(raw_path,new_path,show=True):
 def rm_file(raw_path,show=True):
     command = r'rm -f ' + raw_path
     run_shell(command,show)
-    
+
 def backup_file(Initial_dir,file):
     if not os.path.exists(Initial_dir):
         os.mkdir(Initial_dir)
@@ -86,3 +86,48 @@ def reset_file(Initial_dir,file):
     return False
 
 
+def print_config(config):
+    descriptions={'fix':u'执行加固','check':u'加固检测','repair':u' 还原加固内容 ','backup_path':u'备份目录'}
+    table = PrettyTable(['config', 'set','description'])
+    for c in vars(config):
+        value=getattr(config, c)
+        if type(value)==type([]):
+            description=u'操作内容，空表示所有'
+            table.add_row([c, value, description])
+        else:
+            if c in descriptions.keys():
+                table.add_row([c, value,descriptions[c]])
+            else:
+                try:
+                    table.add_row([c, value, descriptions[value]])
+                except:
+                    table.add_row([c, value, ''])
+    print_line()
+    print("设置项描述")
+    print(table)
+
+def print_desc(fixed_items):
+    table = PrettyTable(['id', 'item', 'description'])
+    for id in fixed_items.keys():
+        table.add_row([id,fixed_items[id][1],fixed_items[id][0].description])
+    print_line()
+    print("加固项描述")
+    print(table)
+    print_line()
+
+def print_line(sym=True):
+    if sym:
+        print('=====================================================================')
+    else:
+        print('---------------------------------------------------------------------')
+
+
+def cprint(string,color):
+    if color=='red':
+        print("\033[31;1m"+string+"\033[0m")
+    if color=='yellow':
+        print("\033[33;1m"+string+"\033[0m")
+    if color=='blue':
+        print("\033[34;1m"+string+"\033[0m")
+    if color=='green':
+        print("\033[36;1m"+string+"\033[0m")
