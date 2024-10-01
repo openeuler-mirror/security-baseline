37a08dd57e7e85c468ba41d5aaeb8d255dc2c4fc
wuksh <wuksh@chinatelecom.cn>
2024-10-01 18:37:13 +0800

    Introduction of the basic structure of the service account module.

---
+++ security-baseline/service_fixed.py
@@ -0,0 +1,10 @@
+# 服务相关加固项，编号从21开始
+import os.path
+
+from do_shell import run_shell
+from base_function import split_file_by_line,grep_find,sed_repalce,append_line,remove_line
+from base_function import rm_file,cp_file,backup_file,reset_file,BaseFix
+from base_function import replace_line,comment_out_line
+from config import fixed_config
+config=fixed_config()
+Initial_dir=config.Initial_dir
